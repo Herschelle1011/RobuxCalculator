f1041c61bb50fcfb8b861cfdf3d1d3332d339c4b
DESKTOP-L65AN15\Admin <herschellee101@gmail.com>
2025-11-23 19:50:19 +0800

    adjusting (responsiveness)

--- javascript.js
+++ javascript.js
@@ -3,10 +3,18 @@ function AmountValue(){
          let totalAfterTax = Math.floor(value * 0.70); 
     let InputFormatted = Number(totalAfterTax).toLocaleString('en');
   let robuxBeforeTax = document.getElementById('beforeTaxVal');
+  let rbxIcon = document.querySelector('.rbxIcon');
+    rbxIcon.style.opacity = 1;
+
+if(value.length === 0){
+    rbxIcon.style.opacity = 0;
+    robuxBeforeTax.textContent = '';
+    return;
+  }
 
 robuxBeforeTax.textContent = InputFormatted;
   };
 
 
-  
+
 
